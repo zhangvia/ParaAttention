edb740ba7592f05e6b43bb4cf3bf1a5fd19c1efe
chengzeyi <ichengzeyi@gmail.com>
2024-11-16 15:33:19 +0800

    add parallelize_transformer function

--- src/para_attn/context_parallel/diffusers_adapters/__init__.py
+++ src/para_attn/context_parallel/diffusers_adapters/__init__.py
@@ -3,6 +3,20 @@ import importlib
 from diffusers import DiffusionPipeline
 
 
+def parallelize_transformer(transformer, *args, **kwargs) -> None:
+    transformer_cls_name = transformer.__class__.__name__
+    if transformer_cls_name.startswith("Flux"):
+        adapter_name = "flux"
+    elif transformer_cls_name.startswith("Mochi"):
+        adapter_name = "mochi"
+    else:
+        raise ValueError(f"Unknown transformer class name: {transformer_cls_name}")
+
+    adapter_module = importlib.import_module(f".{adapter_name}", __package__)
+    parallelize_transformer_fn = getattr(adapter_module, "parallelize_transformer")
+    parallelize_transformer_fn(transformer, *args, **kwargs)
+
+
 def parallelize_pipe(pipe: DiffusionPipeline, *args, **kwargs) -> None:
     assert isinstance(pipe, DiffusionPipeline)
 
